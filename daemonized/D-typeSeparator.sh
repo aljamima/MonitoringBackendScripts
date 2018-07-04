b28d4d4c7a215a66f512e51d1d47d20d42109e84
aljamima <shawnmullaney@hotmail.com>
2018-07-03 20:11:28 -0700

    newSeperator

--- daemonized/D-typeSeparator.sh
+++ daemonized/D-typeSeparator.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 sudo -v
-#### START SCRIPTING | tr -d '\0'
-####            var=$(cat somefile) is more efficiently written var=$(<somefile)
-
-##
 ##    WE CAN SEPERATE BY SOFTWARE MINER IS RUNNING... 'bmMiner' for S9, 'gmMiner' for Genesis GPU, 'cgMiner' for the L3
 ##
 rm -f results.csv 2>/dev/null
@@ -18,15 +14,8 @@ rm -f /tmp/cgMinerIpsMacs.txt 2>/dev/null
 touch /tmp/cgMinerIpsMacs.txt
 rm -f /tmp/notMinerMacs.txt 2>/dev/null
 touch /tmp/notMinerMacs.txt
-if [ -z ${1+x} ]; then
-        echo "Starting To Scan 10.2.1.1/22 Range"
-	sudo fping -a -g 10.2.1.1/22 2>/dev/null > results.csv
-else
-        echo "Starting To Scan $1 Range"
-	sudo fping -a -g $1 2>/dev/null > results.csv
-fi
-echo "Finished Scan, Checking Miner Types"
-sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 results.csv > ips.sorted
+
+sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 ipList.txt > ips.sorted
 for server in $(<ips.sorted); do
 	APISTATS=$(echo -n "stats" | nc -w 1 $server 4028 2>/dev/null | tr -d '\0')
 	DESCR=$(echo $APISTATS | sed -e 's/,/\n/g' | grep "Description" | cut -d "=" -f2)
@@ -47,7 +36,12 @@ for server in $(<ips.sorted); do
 		echo "$server $MAC" | tee -a /tmp/sgMinerIpsMacs.txt
 		echo "$server" > /tmp/sgminerIps.txt
 	elif [[ $DESCR = $CG* ]]; then 
-#		echo "$server" | tee -a /tmp/cgMinerIps.txt
+		if [ nmap -A $server | grep -i "antminer" ]; then
+			echo "$server" | tee -a /tmp/dragonMintIps.txt
+			echo "$server $MAC" | tee -a /tmp/dragonMintIpsMacs.txt
+			continue
+		fi
+		echo "$server" | tee -a /tmp/cgMinerIps.txt
 		echo "$server $MAC" | tee -a /tmp/cgMinerIpsMacs.txt
 		echo "$server" > /tmp/cgminerIps.txt
 	else
@@ -56,7 +50,5 @@ for server in $(<ips.sorted); do
 		echo "$server" > /tmp/notMinerIps.txt
 	fi
 done
-#sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 fpingOutput > fping.sorted
-#diff --changed-group-format="%>" --unchanged-group-format="" "zmap.sorted" "fping.sorted"
 
 wc -l /tmp/*.txt
