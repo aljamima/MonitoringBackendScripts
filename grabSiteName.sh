59ba3bc30fa572ab8a656b67c19b2437734734fc
shawn mullaney <shawnmullaney@hotmail.com>
2018-07-17 08:43:37 -0700

    beta Testing...

--- grabSiteName.sh
+++ grabSiteName.sh
@@ -2,5 +2,5 @@
 export SSHPASS='poopoo11'
 grepper=$1
 
-hostname=$(sshpass -e ssh script@10.0.0.5 hostname)
+hostname=$(sshpass -e ssh -o StrictHostKeyChecking=no script@10.0.0.5 hostname)
 echo "$hostname"
